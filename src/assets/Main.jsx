bb3212423792f9018c5ef936911eb98b5a5a7106
ALatronico <gabant47@gmail.com>
2025-01-28 23:59:23 +0100

    end exercise

---
+++ src/assets/Main.jsx
@@ -0,0 +1,94 @@
+import PostCard from "./PostCard"
+import FormBlog from "./FormBlog"
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+const Main = () => {
+
+
+    const defaultFormData = {
+        title: '',
+        image: '',
+        content: '',
+        tags: [],
+        authorize: false
+    }
+
+    const baseApi = 'http://localhost:3000'
+    const [posts, setPosts] = useState([])
+    const [formData, setFormData] = useState(defaultFormData)
+
+
+    const handlerDeletePost = (id) => {
+
+        axios.delete(`${baseApi}/posts/${id}`)
+            .then(res => {
+                fetchPosts()
+            })
+    }
+    const handlerChange = (e) => {
+
+        let { name, value, type } = e.target;
+
+        if (type == 'checkbox') { value = e.target.checked }
+        if (type == 'select-one') { value = categories[e.target.value] }
+
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    const handlerAddPost = (e) => {
+
+        e.preventDefault();
+        console.log(formData)
+
+        const newPost = { ...formData }
+
+
+        //chiamata all'API ed invio il nuovo elemento
+        axios.post(`${baseApi}/posts`, newPost)
+            .then(res => {
+                //aggiorno lo stato in base alla response del server
+                setPosts([res.data, ...posts]);
+                //resetto il form
+            })
+
+    }
+
+    const fetchPosts = () => {
+        axios.get(`${baseApi}/posts`)
+            .then(res => {
+                console.log(res.data)
+                setPosts(res.data)
+            })
+            .catch(error => {
+                console.error('Errore durante il caricamento delle pizze:', error)
+            })
+    }
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
+
+    return (
+        <main>
+            <div className="container">
+                <FormBlog handlerChange={handlerChange} handlerAddPost={handlerAddPost} formData={formData} />
+            </div>
+            <div className="container border border-primary my-5 rounded">
+                <h1 className="text-center">Elenco post</h1>
+                <div className="row text-center">
+                    {posts.map(post => (
+                        <PostCard key={post.id} post={post} onDelete={handlerDeletePost} />
+                    ))}
+
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Main
---
+++ src/assets/PostCard.jsx
@@ -0,0 +1,22 @@
+const PostCard = (props) => {
+  const { id, title, content, image, tags } = props.post
+  const handlerDeletePost = props.onDelete
+  return (
+      <div className="col-12 col-md-6 col-lg-4 my-4">
+          <div className="card border border-success">
+              <img src={image} className="card-img-top" alt={title} />
+              <div className="card-body">
+                  <h5 className="card-title">{title}</h5>
+                  <p className="card-text text-start">{content}</p>
+                  <button
+                      type="button"
+                      className="btn btn-danger text-start"
+                      onClick={() => handlerDeletePost(id)}
+                  ><i className="fa-solid fa-eraser text-dark pe-3"></i>Elimina post</button>
+              </div>
+          </div>
+      </div >
+  )
+}
+
+export default PostCard
